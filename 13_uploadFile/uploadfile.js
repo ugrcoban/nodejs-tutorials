6aeee39b7c17b4d8cbb7030f69f3367113925439
UGUR COBAN <49412652+ugrcoban@users.noreply.github.com>
2019-04-22 12:26:21 +0300

    Add files via upload

---
+++ 13_uploadFile/uploadfile.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+
+//C:\Users\Your Name>npm install formidable
+var formidable = require('formidable');
+var filename='./uploaded/';
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = filename + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+	console.log('localhost:8080');
+    return res.end();
+  }
+}).listen(8080);
+
+//localhost:8080
